d4d1fd292786035a1545207fcf497a9d43c19cc4
Jarvis <Jarvis@192.168.0.7>
2020-05-09 16:15:43 +0530

    Removing comments

--- src/brickbreaker/Main.java
+++ src/brickbreaker/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		JFrame frame=new JFrame();
 		play pl=new play();
 		
